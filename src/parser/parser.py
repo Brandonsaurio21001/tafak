d859eadbd3647880836a826be920a6f2cda2fb6a
Brandonsaurio21001 <brandonbl2001@estudiantec.cr>
2025-12-09 01:39:58 -0600

    Continuación dle parser
    Se crean las funciones del parser para construir los nodos del AST segun se definen en nodes.py

--- src/parser/parser.py
+++ src/parser/parser.py
@@ -57,6 +57,98 @@ class Parser:
     # declaraciones (funciones, vars)
     # -------------------------
 
+
+    def parse_function(self):
+        """Vamos a construir el nodo de fucnión que definimos en nodes.py
+        FunctionDecl
+        ├── name: "doble"
+        ├── return_type: "chátené"
+        ├── params:
+        │     └── ("chátené", "x")
+        └── body: Block
+                └── statements:
+                        └── ReturnStmt
+                                └── BinaryOp
+                                    ├── left: Variable("x")
+                                    ├── op: "*"
+                                    └── right: Literal(2)
+
+        """
+        #consumir macorroca
+        self.expect(TokenType.KEYWORD, "se esperaba 'macorróca'")
+        #construir el tipo de retonro
+        ret_type = self.expect(TokenType.TYPE, "Se esperaba tipo de retorno").value
+        #constuir el nombre de la funcion
+        name = self.expect(TokenType.IDENT)
+        #los argumentos
+        self.expect(TokenType.SYMBOL, "se esperaba '('")
+        params = self.parse_parameters()
+        self.expect(TokenType.SYMBOL, "se esperaba ')'")
+        #armar el cuerpo
+        body = self.parse_block()
+
+        return FunctionDecl(name, params, ret_type, body)
+
+
+    def parse_parameters(self):
+        params = []
+        # caso de que no lleve parametros, como el main()
+        if self.peek().value == ")":
+            return params
+        
+        while True:
+            #tipo
+            t = self.expect(TokenType.TYPE, "se esperaba tipo de parámetro").value
+            #nombre
+            n = self.expect(TokenType.IDENT, "se esperaba un nombre de parámetro").value
+
+            params.append((t,n))
+
+            #si hay una coma, seguimos
+            if self.peek().value == ",":
+                self.advance()
+                continue
+
+            break
+        return params
+    
+    def parse_block(self):
+        self.expect(TokenType.SYMBOL, "se esperaba '{'")
+        statements = []
+        #vamonos hasta }
+        while not (self.peek().type == TokenType.SYMBOL and self.peek().value == "}"):
+            statements.append(self.parse_statement())
+        
+        self.expect(TokenType.SYMBOL, "se esperaba un '}'")
+        return Block(statements)
+    
+    def parse_var_decl(self):
+        #consumir ti
+        self.expect(TokenType.KEYWORD, "se esperaba 'ti'")
+        #ver el tipo
+        var_type = self.expect(TokenType.TYPE, "se esperaba tipo de variable").value
+        #nombre
+        name = self.expect(TokenType.IDENT, "se esperaba nombre de variable").value
+
+        initializer = None
+        if self.peek().value == "=":
+            self.advance()
+            initializer = self.parse_expression()
+
+        self.expect(TokenType.SYMBOL, "se esperaba ';'")
+
+        return VarDecl(name, var_type, initializer)
+
+
+
+
+
+
+
+
+
+
+
     def parse_declaration(self):
         tok = self.peek()
         
@@ -82,3 +174,6 @@ class Parser:
 
     def parse_expression(self):
         raise NotImplementedError
+
+
+
